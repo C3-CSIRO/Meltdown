a501dad1590d980e8e20ef01513e365f988c8107
nrosa <itodler@gmail.com>
2015-06-29 15:26:32 +1000

    added to DsfWell
    finished writing code for the init and a couple of other method in the
    DsfWell class

--- restructure/DsfWell.py
+++ restructure/DsfWell.py
@@ -2,19 +2,80 @@
 
 
 class DsfWell:
-    def __init__(self,fluorescence,temperatures,cv1,cv2,ph,dphdt,isControl):
+    def __init__(self,fluorescence,temperatures,name,cv1,cv2,ph,dphdt,isControl):
         self.fluorescence = fluorescence
         self.temperatures = temperatures
         self.contents = Contents(cv1,cv2,ph,dphdt,isControl)
+        self.name = name
+
+        self.normalisationFactor = None
+        self.wellMax = None
+        self.wellMin = None
+        self.wellNormalisedMax = None 
+        self.wellNormalisedMin = None
+        self.tm = None
+
+        self.isMonotonic = False
+        self.isComplex =  False
+        self.isOutlier = False
+        self.isInTheNoise = False
+        self.isSaturated = False
+        self.isDiscarded = False
+
         return
     
     def getMinAndMax():
-        return
+        minimum = fluorescence[0]
+        maximum = fluorescence[0]
+        for value in fluorescence[1:]:
+            if value > maximum:
+                maximum = value
+            if value < minimum:
+                minimum = value
+        return (minimum,maximum)
     
     def normalise():
+        count = 0
+        for height in self.fluorescence:
+            count += height*stepSize
+        self.fluorescence = [x / count for x in self.fluorescence]
+        #used to calculate the monotenicity threshold
+        self.normalisationFactor = count
         return
     
     def computeSaturation():
+        if not self.isDsicarded:
+            currentValue = self.fluorescence[0]
+            maxInd = 0
+            while currentValue != self.wellNormalisedMax:
+                maxInd+=1
+                currentValue = self.fluorescence[maxInd]
+
+            diff = self.wellNormalisedMax - self.wellNormalisedMin
+
+            # A boundry defining how much the points can fluctuate and still be considered flat
+            lowFlatBoundry = self.wellNormalisedMax - 0.005*diff
+
+            # Look each way to see how many temperature steps the curve stays flat for
+            count = 0
+            ind = maxInd - 1
+            while ind>=0:
+                if self.fluorescence[ind] > lowFlatBoundry:
+                    count += 1
+                    ind -= 1
+                else:
+                    break
+            ind = maxInd+1
+            while ind<len(self.fluorescence):
+                if self.fluorescence[ind] > lowFlatBoundry:
+                    count += 1 
+                    ind += 1
+                else:
+                    break
+            if count >= 10:
+                self.isSaturated = True
+                self.isDiscarded = True
+
         return
     
     def computeMonotonicity(plateMonotonicThreshold):
