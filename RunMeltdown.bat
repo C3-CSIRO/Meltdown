068b6c99f341e80e4ac8c673f9fa5f8b6a5cf154
Rosa, Nick (Manufacturing, Parkville) <Nick.Rosa@csiro.au>
2016-10-10 10:47:39 +1100

    Added check so batch file will work with anaconda and anaconda2

--- RunMeltdown.bat
+++ RunMeltdown.bat
@@ -1,2 +1,6 @@
+IF EXIST C:\Anaconda\python.exe (
 C:\Anaconda\python.exe %~dp0\source\Meltdown.py
+) ELSE (
+C:\Anaconda2\python.exe %~dp0\source\Meltdown.py
+)
 pause
--- RunMeltdownBatch.bat
+++ RunMeltdownBatch.bat
@@ -1,2 +1,6 @@
+IF EXIST C:\Anaconda\python.exe (
 C:\Anaconda\python.exe %~dp0\source\MeltdownBatch.py
+) ELSE (
+C:\Anaconda2\python.exe %~dp0\source\MeltdownBatch.py
+)
 pause
