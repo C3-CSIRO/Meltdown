3d8e11106433f0eaaca7ed3894b2aa18bd0dda7d
Gu <gu010@csiro.au>
2017-02-21 16:05:01 +1100

    Basically gave it a complete makeover. Mechanism for assigning species completely different.
    Signed-off-by: Gu <gu010@csiro.au>

--- Plate Runner/PlateRunner384.py
+++ Plate Runner/PlateRunner384.py
@@ -1,8 +1,38 @@
+"""
+PLATE RUNNER V.2
+
+
+                   ***      ***                                 
+                  ****      ****                                
+            ------****------****------                          
+           -*************************-                          
+           -*************************-         *                
+           -*************************-        *                 
+           **************************-        *                 
+          *-*****************************    *                  
+         * -*************************-   ****                   
+        *  -*************************-                          
+         * -*************************-                          
+         * ----------*-------*--------                          
+          *         *         **                                
+           *       *            **                              
+                  *              *                             
+                 *              *                              
+    ***         *              *                               
+      ******   *              *                               
+            ***              *                                
+                            *                                 
+                           *                                 
+                          *                                  
+
+"""
+
 from Tkinter import *
 import tkFileDialog
 import tkMessageBox
 import tkFont
 import ttk
+import re
 alph="ABCDEFGHIJKLMNOP"
 
 class PlateRunner:
@@ -12,10 +42,11 @@ class PlateRunner:
 
 		self.wells = {}
 		self.wells_chem = {}
-		self.scale = 40
+		self.scale = 50
 		self.species = []
 
 		#plate related things
+		#self.mainplate = Frame(master)
 		self.plate = Canvas(master, width=self.scale*12, height=self.scale*8, bd=0, highlightthickness=0)
 		self.platerefalph = Canvas(master, width=self.scale/2, height=self.scale*8, bd=0, highlightthickness=0)
 		self.platerefnum=Canvas(master, width=self.scale*12, height=self.scale/2, bd=0, highlightthickness=0)
@@ -36,20 +67,26 @@ class PlateRunner:
 		self.plate.bind("<B1-Motion>", self.platemousemove)
 		self.plate.bind("<Control-1>", self.platectrl)
 		
+		#creating physical display of plate
 		self.plate_create()		
-		
-		self.platerefalph.grid(row = 1, column = 0, rowspan = 8, sticky = E)
-		self.platerefnum.grid(row = 0, column = 1, columnspan = 12, sticky = S)
-		self.plate.grid(row = 1, column = 1, rowspan = 8, columnspan = 12, sticky = NW)
+		self.platerefalph.grid(row = 1, column = 0, sticky = E)
+		self.platerefnum.grid(row = 0, column = 1, columnspan = 3, sticky = S)
+		self.plate.grid(row = 1, column = 1, rowspan = 1, columnspan = 3, sticky = NW)
 
+		#creating empty space
 		self.empty1 = Canvas(master, width = self.scale, height = self.scale/2, bd=0, highlightthickness=0)
 		self.empty1.create_rectangle(0,0,self.scale, self.scale, width=0)
-		self.empty1.grid(row=9, column=1, sticky=W)
+		self.empty1.grid(row=3, column=1, sticky=W)
+
+		self.empty2 = Canvas(master, width = self.scale, height = self.scale/2, bd=0, highlightthickness=0)
+		self.empty2.create_rectangle(0,0,self.scale, self.scale, width=0)
+		self.empty2.grid(row=6, column=1, sticky=W)
+
 
 		#quadrant related things
 		self.quadrant = Canvas(master, width=self.scale*3.6, height=self.scale*3.6, bd=0, highlightthickness=0)
 		self.quadrant_create(384)
-		self.quadrant.grid(row=10, column = 5, rowspan = 8, columnspan = 8, sticky = E)
+		self.quadrant.grid(row=4, column = 3, sticky = NE)
 		
 		self.quadrant.xd=0
 		self.quadrant.yd=0
@@ -58,26 +95,89 @@ class PlateRunner:
 		self.quadrant.bind("<Button-1>", self.quadmousedown)
 		self.quadrant.bind("<Control-1>", self.quadctrl)
 
-		#content key related things
-		Label(master, text = "Enter ident:").grid(row = 10, column = 1, sticky = W)
 
-		self.ass_input = Entry(master)
-		self.ass_input.grid(row = 11, column = 1, columnspan = 4, sticky = W)
+
+
+
+		#Variable 1 tree
+		self.var1frame = Frame(master)
+
+		self.var1 = ttk.Treeview(self.var1frame, columns = ["v1"], show = "headings")
+		self.var1.heading("v1", text = "Variable 1")
+		self.var1.column("v1", width = int(self.scale*3))
+
+		self.var1.insert("", "end", values = ('"*No Variable 1*"'))
+
+		self.vsb1 = ttk.Scrollbar(master, orient="vertical", command=self.var1.yview)
+		self.hsb1 = ttk.Scrollbar(master, orient="horizontal", command=self.var1.xview)
+		self.var1.configure(yscrollcommand=self.vsb1.set, xscrollcommand=self.hsb1.set)
+		self.var1.grid(sticky=NSEW)
+		self.vsb1.grid(column=1, row=0, sticky= NS, in_ = self.var1frame)
+		self.hsb1.grid(column=0, row=1, sticky= EW, in_ = self.var1frame)
+
+		self.var1_input = Entry(self.var1frame)
+		self.var1_input.grid(row = 2, columnspan = 4, sticky = W)
+
+		self.var1_btn=Button(self.var1frame, text="Add Variable 1", command=self.updatevar1)
+		self.var1_btn.grid(row = 3, columnspan = 4, sticky = W)
+
+		self.var1frame.grid(row = 4, column=1, rowspan = 2, sticky = NW)
+
+
+		#Variable 2 tree
+		self.var2frame = Frame(master)
+		self.var2 = ttk.Treeview(self.var2frame, columns = ["v2"], show = "headings")
+		self.var2.heading("v2", text = "Variable 2")
+		self.var2.column("v2", width = int(self.scale*3))
+		
+		self.var2.insert("", "end", values = ('"*No Variable 2*"'))
+
+		self.vsb2 = ttk.Scrollbar(master, orient="vertical", command=self.var2.yview)
+		self.hsb2 = ttk.Scrollbar(master, orient="horizontal", command=self.var2.xview)
+		self.var2.configure(yscrollcommand=self.vsb2.set, xscrollcommand=self.hsb2.set)
+		self.var2.grid(sticky=NSEW)
+		self.vsb2.grid(column=1, row=0, sticky= NS, in_ = self.var2frame)
+		self.hsb2.grid(column=0, row=1, sticky= EW, in_ = self.var2frame)
+
+		self.var2_input = Entry(self.var2frame)
+		self.var2_input.grid(row = 2, columnspan = 4, sticky = W)
+
+		self.var2_btn=Button(self.var2frame, text="Add Variable 2", command=self.updatevar2)
+		self.var2_btn.grid(row = 3, columnspan = 4, sticky = W)
+
+		self.var2frame.grid(row = 4, column = 2, rowspan = 2, sticky = N)
+
+
+		#pH, dpH/dT and Control
+		self.otherinputs = Frame(master)
+
+		Label(self.otherinputs, text = "pH:").grid(row = 11, column = 9, sticky = SW)
+		Label(self.otherinputs, text = "dpH/dT:  ").grid(row = 12, column = 9, sticky = W)
+		Label(self.otherinputs, text = "Control?").grid(row = 13, column = 9, sticky = W)
+
+		self.pH_input = Entry(self.otherinputs)
+		self.pH_input.grid(row = 11, column = 10, columnspan = 2, sticky = SW)
+
+		self.dpH_input = Entry(self.otherinputs)
+		self.dpH_input.grid(row = 12, column = 10, columnspan = 2, sticky = W)
+
+		self.ctr = IntVar() 
+		self.chkbtn = Checkbutton(self.otherinputs, variable=self.ctr)
+		self.chkbtn.grid(row=13, column = 10, sticky = W)
+
+		self.otherinputs.grid(row = 5, column = 3, sticky = NE)
+
+
+		#content key related things
 
 		self.assign_btn=Button(master, text="Assign selected wells", command=self.assign)
-		self.assign_btn.grid(row = 12, column = 1, columnspan = 4, sticky = W)
+		self.assign_btn.grid(row = 7, column = 1, sticky = W)
 
 		self.clear_btn=Button(master, text="Clear selected wells", command=self.unassign)
-		self.clear_btn.grid(row = 13, column = 1, columnspan = 4, sticky = W)
+		self.clear_btn.grid(row = 8, column = 1, sticky = W)
 
-		self.key_btn=Button(master, text="View/Edit Content Key", command=self.editkey)
-		self.key_btn.grid(row = 10, column = 4, columnspan = 4, sticky = W)
-
-		self.load_btn=Button(master, text="Load Content Key", command=self.load)
-		self.load_btn.grid(row = 11, column = 4, columnspan = 4, sticky = W)
-
-		self.create_btn=Button(master, text="Create Content Map", command=self.create_map)
-		self.create_btn.grid(row = 15, column = 4, columnspan = 4, sticky = W)
+		self.create_btn=Button(master, text="CREATE CONTENT MAP", command=self.create_map)
+		self.create_btn.grid(row = 8, column = 3, sticky = E)
 
 		self.empty2=Canvas(master, width=self.scale*0.5, height=self.scale*0.5, bd=0, highlightthickness=0)
 		self.empty2.grid(row=0, column=13)
@@ -86,6 +186,29 @@ class PlateRunner:
 		self.master.bind("<Delete>", self.welldel)
 		
 	#create plate graphic
+
+	def updatevar1(self):
+		self.var1.insert("", "end", values = (re.escape(self.var1_input.get())))
+		self.var1_input.delete(0,END)
+
+	def updatevar2(self):
+		self.var2.insert("", "end", values = (re.escape(self.var2_input.get())))
+		self.var2_input.delete(0,END)
+
+	def readvar(self, x):
+		if x == 1:
+			curItem = self.var1.focus()
+			try:
+				return str(self.var1.item(curItem)['values'][0])
+			except:
+				return ""
+		elif x == 2:
+			curItem = self.var2.focus()
+			try:
+				return str(self.var2.item(curItem)['values'][0])
+			except:
+				return ""
+
 	def plate_create(self):
 		for c in range(12):
 			self.platerefnum.create_text(self.scale*(2*c+1)/2.0, self.scale/4, text=str(c*2+1)+"/"+str(c*2+2))
@@ -217,23 +340,18 @@ class PlateRunner:
 	def assign(self):
 		if self.plate.selected == [] or self.quadrant.selected == []:
 			tkMessageBox.showwarning("Error", "No wells selected.")
-		if self.plate.legend=={}:
-			tkMessageBox.showwarning("Error", "No idents exist. Consider loading a content key file.")
+		elif self.readvar(1) == "":
+			tkMessageBox.showwarning("Error", "No Variable 1 selected to assign.")
 		else:
-			y=self.ass_input.get()
-			if y not in self.plate.legend:
-				tkMessageBox.showwarning("Error", "Ident does not exist you wet napkin!!")
+			if self.ctr.get() == 1:
+				x="1"
 			else:
-				for i in self.plate.selected:
-					for k in self.quadrant.selected:
-						if k%2==1:
-							self.quadrant.itemconfig(k+1, text=y)
-							to_add = ""
-							for j in self.plate.legend[y]:
-								to_add+=j+"\t"
-							self.wells_chem[i+50*(k-1)]=to_add[:-2]
-							self.quads_chem[i+50*(k-1)]=y
-		self.ass_input.delete(0,END)
+				x=""
+			for i in self.plate.selected:
+				for k in self.quadrant.selected:
+					if k%2==1:
+						self.wells_chem[i+50*(k-1)]=[self.readvar(1), self.readvar(2), self.pH_input.get(), self.dpH_input.get(), x]
+						self.quads_chem[i+50*(k-1)]=self.readvar(1)[:6]
 
 	def unassign(self):
 		for i in self.plate.selected:
@@ -259,10 +377,11 @@ class PlateRunner:
 			content_str+=xyz
 			content_str+="\t"
 			if self.wells_chem[i]!="":
-				content_str+=self.wells_chem[i]
+				for j in self.wells_chem[i]:
+					content_str+=j+"\t"
 			else:
-				content_str+="EMPTY\t\t\t\t"
-			content_str+="\n"
+				content_str+="EMPTY\t\t\t\t\t"
+			content_str = content_str[:-1]+"\n"
 		gname=tkFileDialog.asksaveasfilename(title = "Save file as", filetypes = (('text files', '.txt'), ('all files', '.*')))
 
 		if gname != "":
@@ -275,7 +394,9 @@ class PlateRunner:
 		else:
 			tkMessageBox.showwarning("Error", "Failed to save content map.")
 
+
 	#open content key window
+	"""
 	def load(self):
 		self.plate.legend_raw=[]
 		f = tkFileDialog.askopenfile(mode='r', **{'filetypes':[('all files', '.*'), ('text files', '.txt')]})
@@ -379,8 +500,8 @@ class PlateRunner:
 
 	def build_tree(self):
 		for col in self.map_header:
-			self.tree.heading(col, text=col.title(), command=lambda c=col: self.sortby(self.tree, c, 0))
-			self.tree.column(col, width=tkFont.Font().measure(col.title()))
+			self.tree.heading(col, text=col, command=lambda c=col: self.sortby(self.tree, c, 0))
+			self.tree.column(col, width=tkFont.Font().measure(col))
 
 		for item in self.species:
 			self.tree.insert('', 'end', values=item)
@@ -419,6 +540,7 @@ class PlateRunner:
 
 	def lol_fnc_inf(self, event):
 		self.add_now_to_key()
+	"""
 
 root = Tk()
 
